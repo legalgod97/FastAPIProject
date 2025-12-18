2098561b38fc9733c1e415b2dd9bf586d2656798
saveliy <boev@r-proc.ru>
2025-12-18 18:15:14 +0300

    "Initial commit" - removed files again

--- src/exceptions/http.py
+++ src/exceptions/http.py
@@ -1,6 +1,6 @@
 from fastapi.responses import UJSONResponse
 from starlette import status
-from src.exceptions.common import NotFoundError
+from src.exceptions.common import NotFoundError, ValidationError
 
 
 def register_exception_handlers(app):
@@ -11,4 +11,12 @@ def register_exception_handlers(app):
             content={
                 "detail": f"{exc.entity} not found",
             },
+        )
+
+
+    @app.exception_handler(ValidationError)
+    async def validation_handler(request, exc: ValidationError):
+        return UJSONResponse(
+            status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+            content={"detail": f"{exc.entity} validation failed"},
         )
